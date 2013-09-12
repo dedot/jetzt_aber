8595a6d246be78c2ff07211f73210da172a5282d
Stefan Schmitt <stefan.c.schmitt@gmail.com>
2013-09-12 21:21:20 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JetztAber::Application.config.secret_key_base = '96b41d3c11ecc01fd5faf2d1b2969e2f588cb19e03f03ef475c48db97c86610b112b719df2fd6983ae4ceedc246b5cf2971b6d7000cc8d816b3af19beaa81d3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JetztAber::Application.config.session_store :cookie_store, key: '_jetzt_aber_session'
